2d754f8719f6e3427a690579abf6372e9cedde49
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2019-04-13 16:33:00 -0400

    Update 0.3.2
    Change colour mechanism and debugging!

--- jsmain.js
+++ jsmain.js
@@ -2,11 +2,35 @@
 var x = document.querySelector("div.TestArea");
 var y = document.querySelector("p#Start");
 var w = Math.floor(Math.random() * 5) + 1;
-var click = 0;
+var colorName;
+var oldName;
 
 
+//Function that allows me to switch classes and color
+function ChangeClassColor(colorName, oldName){
+  x.classList.add(colorName);
+  x.classList.remove(oldName);
+  x = document.querySelector("div" + '.' + colorName);
 
-x.onclick = function Start(){
-  x.style.backgroundColor = "red";
-  y.innerHTML = "Wait for the green to appear!";
+
+  console.log(colorName);
+  return colorName;
+}
+
+var colorName = ChangeClassColor("TestAreaBlue", "TestArea");
+//Function that switch for Blue to Red
+
+x.onclick = function switchColor(){
+  if (colorName == "TestAreaBlue"){
+      ChangeClassColor("TestAreaRed", "TestAreaBlue");
+      y.innerHTML = "Wait for the green to appear!";
+
+  }
+
+  else if (colorName == "TestAreaRed") {
+      ChangeClassColor("TestAreaBlue", "TestAreaRed");
+      y.innerHTML = "Click too early, try again!";
+
+  
+    }
 }
