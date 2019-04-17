ca1f8941b4806af234c81e333c86be13afe004fa
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2019-04-16 20:43:29 -0400

    0.5.5
    recenter text

--- jsmain.js
+++ jsmain.js
@@ -5,6 +5,22 @@ var w = Math.floor(Math.random() * 5) + 1;
 var colorName;
 var oldName;
 
+//Credit for finding height: https://plainjs.com/javascript/styles/set-and-get-css-styles-of-elements-53/
+//Idea for window resize: https://developer.mozilla.org/en-US/docs/Web/API/Document/defaultView/resize_event
+
+//Function allows for recenter text
+window.addEventListener('resize',function(){
+  var elem  = document.querySelector('div')
+  var style = window.getComputedStyle ? getComputedStyle(elem, null) : elem.currentStyle;
+  var rheight  = innerHeight;
+  rheight = parseInt(rheight);
+  lheight = Math.floor(rheight/50);
+  elem.style.lineHeight = lheight;
+});
+
+
+
+
 
 //Function that allows me to switch classes and color
 function ChangeClassColor(colorName, oldName){
