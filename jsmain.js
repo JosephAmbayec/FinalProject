1b159e686ee5d25f8c88b75f4a3a1db8354e2972
JosephAmbayec <JosephAmbayec@users.noreply.github.com>
2019-04-23 09:20:36 -0400

    april23 version 0.7.2

--- jsmain.js
+++ jsmain.js
@@ -6,21 +6,27 @@ var colorName;
 var oldName;
 var s;
 var e;
+
 //Function to start timer
 function timer(){
   t = e - s;
   return t;
 }
+
 function sTime() {
    s = new Date();
    s.getMilliseconds();
   console.log(s);
   }
+
 function eTime(){
    e = new Date();
    e.getMilliseconds();
   console.log(e);
 }
+
+
+
 //Credit for finding height: https://plainjs.com/javascript/styles/set-and-get-css-styles-of-elements-53/
 //Idea for window resize: https://developer.mozilla.org/en-US/docs/Web/API/Document/defaultView/resize_event
 //Function allows for recenter text
@@ -37,23 +43,25 @@ window.addEventListener('resize',function(){
 });
 
 function changetoRed(){
-  var w = Math.floor((Math.random() * 5) + 1) * 1000;
+  // Picks Random time
+  var w = ((Math.random() * 5) + 2) * 1000;
   ChangeClassColor("TestAreaRed", "TestAreaBlue");
   colorName = "TestAreaRed"
   y.innerHTML = "Wait for the green to appear!";
-  console.log(w);
+ console.log(w);
   setTimeout(function green(){
+    // Only allows for green to appear when not blue
     if (colorName != "TestAreaBlue"){
       sTime()
       ChangeClassColor("TestAreaGreen", "TestAreaRed");
       y.innerHTML = "Click!";
       colorName = "TestAreaGreen";
-      return s
+      return s;
     }
     else {
       console.log("Stopped green function");
     }
-    }, w)
+  }, w)
 
 
 }
@@ -61,7 +69,7 @@ function changetoRed(){
 function changetoBlue(text){
 
   y.innerHTML = text;
-  colorName = "TestAreaBlue"
+  colorName = "TestAreaBlue";
 
 }
 
@@ -78,6 +86,18 @@ function ChangeClassColor(colorName, oldName){
   return oldName
 }
 
+// Function to for achievements
+function achieve(){
+  if (t < 500){
+    console.log("Wow you're fast");
+
+  }
+  else {
+    // Nothing
+  }
+
+}
+
 
 
 var colorName = ChangeClassColor("TestAreaBlue", "TestArea");
@@ -88,14 +108,14 @@ x.onclick = function switchColor(){
  if (colorName == "TestAreaRed" && colorName != "TestAreaBlue") {
       // When its red and clicked then:
       ChangeClassColor("TestAreaBlue", "TestAreaRed");
-      changetoBlue("Clicked too early!")
+      changetoBlue("Clicked too early!");
 
 
   }
  else if (colorName == "TestAreaBlue" && colorName != "TestAreaRed"){
         // When its blue and clicked then:
 
-        changetoRed()
+        changetoRed();
     }
 
  else if(colorName == "TestAreaGreen" && colorName != "TestAreaRed"){
@@ -105,14 +125,19 @@ x.onclick = function switchColor(){
       eTime();
 
       timer();
+      achieve()
       changetoBlue(t + " ms. Click to go again");
       return e;
  }
 
   else{
-    console.log("Nothing")
+    console.log("Nothing");
   }
 }
+
+
+
+
 
 
 
